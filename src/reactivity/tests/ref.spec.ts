f863e07093688d3f387ae019ee6988197f885cbb
wangjr <wangjr@getui.com>
2022-05-22 17:13:34 +0800

    feat: unref和isRef

--- src/reactivity/tests/ref.spec.ts
+++ src/reactivity/tests/ref.spec.ts
@@ -1,5 +1,5 @@
 import { effect } from '../effect';
-import { ref } from '../ref';
+import { isRef, ref, unref } from '../ref';
 
 describe('ref', () => {
   it('should be reactive', () => {
@@ -31,5 +31,20 @@ describe('ref', () => {
     expect(dummy).toBe(1);
     a.value.count = 2;
     expect(dummy).toBe(2);
+  });
+
+  test('isRef', () => {
+    expect(isRef(ref(1))).toBe(true);
+    // expect(isRef(computed(() => 1))).toBe(true);
+
+    expect(isRef(0)).toBe(false);
+    expect(isRef(1)).toBe(false);
+    // an object that looks like a ref isn't necessarily a ref
+    expect(isRef({ value: 0 })).toBe(false);
+  });
+
+  test('unref', () => {
+    expect(unref(1)).toBe(1);
+    expect(unref(ref(1))).toBe(1);
   });
 });
